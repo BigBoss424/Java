62c1809e4dace1f21a07acb09d7d86dfae226623
BigBoss424 <ajones061392@gmail.com>
2014-05-30 09:45:43 -0700

    Did some more practice on memorizing my LinkedList methods

--- LinkedList.java
+++ LinkedList.java
@@ -114,6 +114,31 @@ public class LinkedList
       size++;
    }
    
+   public void add(int value)
+   {
+      Node newNode = new Node(value);
+      Node curr = head;
+      while(curr.next != null)
+      {
+	curr.next = curr;
+      }
+      curr.next = newNode;
+      size++;
+   }
+   
+   public void add(int value)
+   {
+      Node newNode = new Node(value);
+      Node curr = head;
+      while(curr.next != null)
+      {
+        curr.next = curr;
+      }
+      curr.next = newNode;
+      size++;
+   }
+   
+   
    public void delete(int index)
    {
       if(index >= 0 && index < size)
@@ -180,6 +205,45 @@ public class LinkedList
       }
    }
    
+   public void delete(int index)
+   {
+      if(index >= 0 && index < size)
+      {
+	 if(index == 0)
+	 {
+	   head.next = head.next.next;
+	 }
+	 else
+	 {
+	   Node prev = find(index - 1);
+	   Node curr = prev.next;
+	   prev.next = curr.next;
+	 }
+	 size--;
+      }
+      else
+      {
+	throw new IndexOutOfBoundsException("list index out of range): " + index);
+      }
+   }
+   
+   public void delete(int index)
+   {
+      if(index >= 0 && index < size)
+      {
+	if(index == 0)
+	{
+	  head.next = head.next.next;
+	}
+	else
+	{
+	  Node prev = find(index -1);
+	  Node curr = prev.next;
+	  prev.next = curr.next;
+	}
+	size--;
+      }
+   }
    public String toString()
    {
       String s = "";
@@ -248,5 +312,211 @@ public class LinkedList
       s = s + curr.value + " ";
     }
     return s;
+   }
+   
+   public String toString()
+   {
+      String s = "";
+      for(Node curr = head.next; curr != null; curr = curr.next)
+      {
+	s = s + curr.value + "";
+      }
+      return s;
+   }
+   
+   public String toString()
+   {
+    String s = "";
+    for(Node curr = head.next; curr != null; curr = curr.next)
+    {
+      s = s + curr.value + "";
+    }
+    return s;
+   }
+   
+   public String toString()
+   {
+      String s = "";
+      for(Node curr = head.next; curr != null; curr = curr.next)
+      {
+	s = s + curr.value + "";
+      }
+      return s;
+   }
+   
+   public void add(int value)
+   {
+     Node newNode = new Node(value);
+     Node curr = head;
+     while(curr.next != null)
+     {
+	curr = curr.next;
+     }
+     curr.next = newNode;
+     size++;
+   }
+   
+   public void delete(int index)
+   {
+      if(index >= 0 && index < size)
+      {
+	if(index == 0)
+	{
+	  head.next = head.next.next;
+	}
+	else
+	{
+	  Node prev = find(index - 1);
+	  Node curr = prev.next;
+	  prev.next = curr.next;
+	}
+	size--
+      }
+      else
+      {
+	throws new IndexOutOfBoundsException("Index out of bounds: " + index);
+      }
+   }
+   
+   public void addOrdered(int index, int value)
+   {
+     if(index >= 0 && index <= size)
+     {
+	Node newNode = new Node(value);
+	if(index == 0)
+	{	
+	  newNode.next = head.next;
+	  head.next = newNode;
+	}
+	else
+	{
+	  Node prev = find(index - 1);
+	  newNode.next = prev.next;
+	  prev.next = newNode;
+	}
+	size++
+     }
+     else
+     {
+	throws new IndexOutOfBoundsException("Index out of bounds: " + index);
+     }
+   
+   }
+   
+   public void addOrdered(int index, int value)
+   {
+      if(index >= 0 && index <= size)
+      {
+	Node newNode = new Node(value)
+	if(index == 0)
+	{
+	  newNode.next = head.next;
+	  head.next = newNode;
+	}
+	else
+	{
+	  Node prev = find(index -1)
+	  Node curr = prev.next;
+	  prev.next = newNode;
+	}
+	size++;
+      }
+      else
+      {
+	throw new IndexOutOfBoundsException("List index error: " + index);
+      }
+   }
+   
+   public void addOrdered(int index, int value)
+   {
+      if(index >= 0 && index <= size)
+      {
+	Node newNode = new Node(value);
+	if(index == 0)
+	{
+	  newNode.next = head.next;
+	  head.next = newNode;
+	}
+	else
+	{
+	  Node prev = find(index - 1);
+	  Node curr = prev.next;
+	  prev.next = newNode;
+	}size++;
+	
+      }
+      else
+      {
+	throw new IndexOutOfBoundsException("Index out of bounds: " + index);
+      }
+   }
+   
+   public void add(int value)
+   {
+      Node newNode = new Node(value);
+      Node curr = head;
+      
+      while(curr.next != null)
+      {
+	curr = curr.next;
+      }
+      curr.next = newNode;
+      size++;
+   }
+   
+   public void delete(int index)
+   {
+      if(index >= 0 && index < size)
+      {
+	if(index == 0)
+	{
+	  head.next = head.next.next;
+	}
+	else
+	{
+	  Node prev = find(index -1);
+	  Node curr = prev.next;
+	  prev.next = curr.next;
+	}size--;
+	
+      }
+      else
+      {
+	throw new IndexOutOfBoundsException("Index out of bounds: " + index);
+      }
+   }
+   
+   public String toString()
+   {
+      String s = "";
+      for(Node curr = head.next; curr != null; curr = curr.next)
+      {
+	s = s + curr.value + "";
+      }
+      return s;
+   }
+   
+   public void addOrdered(int index, int value)
+   {
+      if(index >= 0 && index <= size)
+      {
+	Node newNode = new Node(value);
+	if(index == 0)
+	{
+	  newNode.next = head.next;
+	  head.next = newNode;
+	}
+	else
+	{
+	  Node prev = find(index -1);
+	  Node curr = prev.next;
+	  prev.next = newNode;
+	}
+	size++;
+      }
+      else
+      {
+	throw new IndexOutOfBoundsException("Index out of bounds: " + index);
+      }
    }
 }//end class
