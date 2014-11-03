24995ee823d9d443a971f082c1d36fa89e53909e
Aaron Jones <BigBoss424@hotmail.com>
2014-11-02 20:39:42 -0800

    Added the file for the Linked List practice in Java for the APE. Will add ways to manipulate later down the line.

---
+++ LinkedListPractice/LinkedLIst.java
@@ -0,0 +1,27 @@
+//Starting the practice of linked lists
+
+public class LinkedLIst
+{
+   private class Node
+   {
+      public int value;
+      public Node next;
+      
+      
+         public Node(int aValue)
+         {
+            this.value = aValue;
+         }
+   }
+
+
+   private Node head;
+   private int size;
+   
+   public LinkedListDummy()
+   {
+      this.head = new Node(0);
+   ]
+}
+
+
