a87f2b0316b749886eb72093d72699bff48b2efe
ajones424 <ajones061392@yahoo.com>
2023-02-11 20:22:51 -0800

    Started a Learning Java tutorial series, for online consumption.

---
+++ Learning_Java/helloWorld.java
@@ -0,0 +1,15 @@
+/* Program: Hello World
+ * Date: 2/11/2023
+ * Description: This program will display "Hello World" in the console once compiled and executed.
+ * Creator: Aaron Jones
+ */
+
+//First we start by declaring the class
+public class helloWorld{
+    
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }//end of the main function
+    
+}//end of helloWorld class
